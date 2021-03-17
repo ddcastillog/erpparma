58b4d1500571415be8cc0302150b6c1e3b60eb3b
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2021-03-17 09:35:56 -0500

    Avances de facturación

--- erpparmaEJB/ejbModule/erpparma/model/facturacion/dtos/DTOClientes.java
+++ erpparmaEJB/ejbModule/erpparma/model/facturacion/dtos/DTOClientes.java
@@ -1,13 +1,77 @@
 package erpparma.model.facturacion.dtos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DTOClientes {
-	
+
 	private String codigo;
 	private String apellidos;
 	private String nombres;
 	private String correo;
-	private String activo;
-	
-	
+	private Boolean activo;
+	List<DTOFactura> facturas;
+
+	public DTOClientes(String codigo, String apellidos, String nombres, String correo, Boolean activo) {
+		this.codigo = codigo;
+		this.apellidos = apellidos;
+		this.nombres = nombres;
+		this.correo = correo;
+		this.activo = activo;
+		this.facturas = new ArrayList<DTOFactura>();
+	}
+
+	public List<DTOFactura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<DTOFactura> facturas) {
+		this.facturas = facturas;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public Boolean getActivo() {
+		return activo;
+	}
+
+	public void setActivo(Boolean activo) {
+		this.activo = activo;
+	}
+
+	@Override
+	public String toString() {
+		return apellidos + " " + nombres;
+	}
 
 }
--- erpparmaEJB/ejbModule/erpparma/model/facturacion/managers/ManagerFacturacion.java
+++ erpparmaEJB/ejbModule/erpparma/model/facturacion/managers/ManagerFacturacion.java
@@ -1,28 +1,78 @@
 package erpparma.model.facturacion.managers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import erpparma.model.core.entities.ParmaProducto;
+import erpparma.model.core.entities.SegAsignacion;
+import erpparma.model.core.entities.SegUsuario;
 import erpparma.model.core.managers.ManagerDAO;
-
+import erpparma.model.facturacion.dtos.DTOClientes;
 
 @Stateless
 @LocalBean
 public class ManagerFacturacion {
 
-	//pass admin1004291447
-	
+	// pass admin1004291447
+
 	@EJB
 	private ManagerDAO mDao;
-   
-    public ManagerFacturacion() {
-       
-    }
-    
-    
-    
-    
-    
+
+	public ManagerFacturacion() {
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<DTOClientes> findAllClientes() {
+		String field = "segModulo";
+		String where = "o.".concat(field).concat("=").concat("'").concat("4").concat("'");
+		List<SegAsignacion> asigs = this.mDao.findWhere(SegAsignacion.class, where, "idSegAsignacion");
+		List<DTOClientes> clientes = new ArrayList<DTOClientes>();
+		for (SegAsignacion asig : asigs) {
+			SegUsuario user = asig.getSegUsuario();
+			DTOClientes cliente = new DTOClientes(user.getCodigo(), user.getApellidos(), user.getNombres(),
+					user.getCorreo(), user.getActivo());
+			clientes.add(cliente);
+		}
+		return clientes;
+	}
+
+	public DTOClientes findClienteById() {
+
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<ParmaProducto> findAllProductosVenta() {
+		String field = "parmaTipoProducto.nombreTipoProducto";
+		String where = "o.".concat(field).concat("=").concat("'").concat("venta").concat("'");
+		List<ParmaProducto> productos = this.mDao.findWhere(ParmaProducto.class, where, "idParmaProducto");
+		return productos;
+	}
+
+	public ParmaProducto findProductoVentaById(Integer id) throws Exception {
+		ParmaProducto p = (ParmaProducto) this.mDao.findById(ParmaProducto.class, id);
+		if (p.getParmaTipoProducto().getNombreTipoProducto() == "venta") {
+			return p;
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public ParmaProducto findProductoVentaByName(String name) throws Exception {
+		String field = "nombreProducto";
+		String where = "o.".concat(field).concat("=").concat("'").concat(name).concat("'");
+		List<ParmaProducto> p = this.mDao.findWhere(ParmaProducto.class, where, "nombreProducto");
+
+		if (p.get(0).getParmaTipoProducto().getNombreTipoProducto().equals("venta")) {
+			System.out.println("Tamaño: " + p.get(0).getParmaTipoProducto().getNombreTipoProducto());
+			return p.get(0);
+		}
+		return null;
+	}
 
 }
