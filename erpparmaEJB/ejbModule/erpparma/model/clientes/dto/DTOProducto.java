3df346566d47d5b6df31a8dc729f709ec66a6d7e
mevalverdey@utn.edu.ec <mevalverdey@utn.edu.ec>
2021-03-23 16:46:49 -0500

    Avance Modulo Clientes

--- erpparmaEJB/ejbModule/erpparma/model/clientes/dto/DTOProducto.java
+++ erpparmaEJB/ejbModule/erpparma/model/clientes/dto/DTOProducto.java
@@ -1,15 +1,40 @@
 package erpparma.model.clientes.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import erpparma.model.core.entities.ParmaProducto;
+
 public class DTOProducto {
 	
 	private int codigo;
 	private String nombre;
 	private double precio;
-	public DTOProducto(int codigo, String nombre, double precio) {
+	private ArrayList<ParmaProducto> productosparma;
 	
+	private ParmaProducto producto;
+	
+	
+	
+	public ArrayList<ParmaProducto> listaDTOProductos(){
+		
+		
+		productosparma = getProductosparma();
+		
+		
+		return productosparma;
+		
+	}
+	
+	public DTOProducto() {
+		
+	}
+	public DTOProducto(int codigo, String nombre, double precio, ArrayList<ParmaProducto> productosparma) {
+		super();
 		this.codigo = codigo;
 		this.nombre = nombre;
 		this.precio = precio;
+		this.productosparma = productosparma;
 	}
 	public int getCodigo() {
 		return codigo;
@@ -29,10 +54,12 @@ public class DTOProducto {
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	
-	
-	
-	
+	public ArrayList<ParmaProducto> getProductosparma() {
+		return productosparma;
+	}
+	public void setProductosparma(ArrayList<ParmaProducto> productosparma) {
+		this.productosparma = productosparma;
+	}
 	
 	
 	
--- erpparmaWeb/src/erpparma/controller/clientes/carrito/BeanCarrito.java
+++ erpparmaWeb/src/erpparma/controller/clientes/carrito/BeanCarrito.java
@@ -10,143 +10,103 @@ import javax.inject.Named;
 
 import erpparma.model.clientes.dto.DTOProducto;
 import erpparma.model.clientes.managers.ManagerClientes;
+import erpparma.model.core.entities.ParmaProducto;
 
 @Named
 @SessionScoped
 public class BeanCarrito implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@EJB
 	private ManagerClientes mClientes;
-	
-	private List<DTOProducto> listaproductos;
-	
-	private List<DTOProducto> carrito;
-	
+
+	private List<ParmaProducto> listaproductos;
+	private List<ParmaProducto> listaproductos1;
+	private List<ParmaProducto> carrito;
 	private double totalcarrito;
-	
+
 	@PostConstruct
-	
-	public void inicializar ()  {
-		
+
+	public void inicializar() {
+
 		listaproductos = mClientes.generardatosProductos();
+
 	}
+
+	public BeanCarrito() {
+		// TODO Auto-generated constructor stub
+	}
+
 	
-	
-	public void actionListenerAgregarCarrito (DTOProducto producto) {
-		
+	public void actionListenerAgregarCarrito(ParmaProducto producto) {
+
 		carrito = mClientes.agregardatosCarrito(carrito, producto);
-		
-		totalcarrito = mClientes.CalcularTotal(carrito);
-		
+
+		totalcarrito = mClientes.CalcularTotal(getCarrito());
+
 	}
 	
-	
-public void actionListenereliminarCarrito (DTOProducto producto) {
-		
-		carrito = mClientes.eliminardatosCarrito(carrito, producto.getCodigo());
-		
+	public void actionListenereliminarCarrito(ParmaProducto producto) {
+
+		carrito = mClientes.eliminardatosCarrito(carrito, producto.getIdParmaProducto());
 
 		totalcarrito = mClientes.CalcularTotal(carrito);
-		
+
+	}
+	
+	public String actionComprar1() {
+		listaproductos = mClientes.generardatosProductos();
+		return "carrito";
+	}
+
+	public List<ParmaProducto> getListaproductos1() {
+		return listaproductos1;
+	}
+
+	public void setListaproductos1(List<ParmaProducto> listaproductos1) {
+		this.listaproductos1 = listaproductos1;
 	}
 
 
+	public String actionComprar() {
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		return "usuarios";
+	}
+
+
 
 	public double getTotalcarrito() {
-	return totalcarrito;
-}
+		return totalcarrito;
+	}
 
+	public void setTotalcarrito(double totalcarrito) {
+		this.totalcarrito = totalcarrito;
+	}
 
-public void setTotalcarrito(double totalcarrito) {
-	this.totalcarrito = totalcarrito;
-}
-
-
-	public List<DTOProducto> getCarrito() {
+	public List<ParmaProducto> getCarrito() {
 		return carrito;
 	}
 
-
-
-
-
-
-
-
-	public void setCarrito(List<DTOProducto> carrito) {
+	public void setCarrito(List<ParmaProducto> carrito) {
 		this.carrito = carrito;
 	}
-
-
-
-
-
-
-
 
 	public ManagerClientes getmClientes() {
 		return mClientes;
 	}
 
-
-
-
-
-
 	public void setmClientes(ManagerClientes mClientes) {
 		this.mClientes = mClientes;
 	}
 
-
-
-
-
-
-	public List<DTOProducto> getListaproductos() {
+	public List<ParmaProducto> getListaproductos() {
 		return listaproductos;
 	}
 
-
-
-
-
-
-	public void setListaproductos(List<DTOProducto> listaproductos) {
+	public void setListaproductos(List<ParmaProducto> listaproductos) {
 		this.listaproductos = listaproductos;
 	}
-	
-	
-	
 
-
-
-
-
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-
-
-
-
-
-	public BeanCarrito() {
-		// TODO Auto-generated constructor stub
-	}
 
 }
